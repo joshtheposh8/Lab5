a3b649bae2ff681a5070fbb4a73dd05b5e12b845
joshtheposh8 <225066180+joshtheposh8@users.noreply.github.com>
2026-05-08 08:04:39 -0600

    sevenseg fix

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -17,23 +17,23 @@ end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 begin
-    with i_hex select
+    with i_Hex select
         o_seg_n <=
-            "1000000" when x"0",
-            "1111001" when x"1",
-            "0100100" when x"2",
-            "0110000" when x"3",
-            "0011001" when x"4",
-            "0010010" when x"5",
-            "0000010" when x"6",
-            "1111000" when x"7",
-            "0000000" when x"8",
-            "0011000" when x"9",
-            "0001000" when x"A",
-            "0000011" when x"B",
-            "1000111" when x"C",
-            "0100001" when x"D",
-            "0000110" when x"E",
-            "0001110" when x"F",
-            "1111111" when others;
+            "1000000" when "0000", -- 0
+            "1111001" when "0001", -- 1
+            "0100100" when "0010", -- 2
+            "0110000" when "0011", -- 3
+            "0011001" when "0100", -- 4
+            "0010010" when "0101", -- 5
+            "0000010" when "0110", -- 6
+            "1111000" when "0111", -- 7
+            "0000000" when "1000", -- 8
+            "0010000" when "1001", -- 9
+            "0001000" when "1010", -- A
+            "0000011" when "1011", -- b
+            "1000110" when "1100", -- C
+            "0100001" when "1101", -- d
+            "0000110" when "1110", -- E
+            "0001110" when "1111", -- F
+            "1111111" when others; 
 end Behavioral;
