72f21f83fb76add2b0fefdf6159823570dc0bf93
joshtheposh8 <225066180+joshtheposh8@users.noreply.github.com>
2026-05-08 07:52:53 -0600

    sevenseg fix

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -10,16 +10,15 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenseg_decoder is
     Port ( 
-        i_hex : in  STD_LOGIC_VECTOR (3 downto 0);
-        o_seg : out STD_LOGIC_VECTOR (6 downto 0)
+        i_hex   : in  STD_LOGIC_VECTOR (3 downto 0);
+        o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
     );
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 begin
-
     with i_hex select
-        o_seg <=
+        o_seg_n <=
             "1000000" when x"0",
             "1111001" when x"1",
             "0100100" when x"2",
@@ -37,5 +36,4 @@ begin
             "0000110" when x"E",
             "0001110" when x"F",
             "1111111" when others;
-
 end Behavioral;
